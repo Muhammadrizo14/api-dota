44a3d90aca0bf3722ca9be2a4203f8b838136099
Muhammadrizo14 <mtursunboev13@gmail.com>
2022-05-19 16:20:58 +0500

    to

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import AppModule from "./app.module";
 
 
+
+
+
 const port = 3001
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors();
-  await app.listen(port);
-  console.log(`Server running on port ${port}`)
+  const server = app.listen(process.env.PORT || 5000, () => {
+    console.log(`Express is working on port 5000`);
+  });
 }
 bootstrap().then(() => {
   console.log('bootstrap loaded :smile:')
